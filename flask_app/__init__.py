8eac7605192e51370559ed7bb7182c54c412a07f
Kenneth Hazlett <hazlettkenneth1@gmail.com>
2022-06-15 18:52:52 -0400

    round3

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -1,5 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 app.secret_key = "Kenny."
+
+
+@app.route("/")
+def index():
+    return render_template("registration.html")
