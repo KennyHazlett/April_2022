2ed7826f4c39f1ee288510371037289dd10f6e3d
Kenneth Hazlett <hazlettkenneth1@gmail.com>
2022-06-17 13:48:34 -0400

    round17

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -1,4 +1,11 @@
 from flask import Flask, render_template
+from flask import render_template, redirect, session, request, flash
+from flask_app.__init__ import app
+from flask_app.models.user import User
+from flask_app.models.dream import Dream
+from flask_bcrypt import Bcrypt
+from db import mysql
+bcrypt = Bcrypt(app)
 app = Flask(__name__)
 
 app.secret_key = "Kenny."
@@ -13,6 +20,18 @@ def home_view():
 def login_page():
     return render_template("login.html")
 
+
+@app.route('/login', methods=['POST'])
+def login():
+    user = User.get_by_email(request.form)
+    if not user:
+        flash("Invalid Email", "login")
+        return redirect('/login')
+    if not bcrypt.check_password_hash(user.password, request.form['password']):
+        flash("Invalid Password", "login")
+        return redirect('/login')
+    session['user_id'] = user.id
+    return redirect('/menu')
 # @app.route("/")
 # def init_view():
 #     return index
