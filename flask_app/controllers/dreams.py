8dea94240cc33fec932229469415dfba712e2def
Kenneth Hazlett <hazlettkenneth1@gmail.com>
2022-06-20 19:18:36 -0400

    round29

--- flask_app/controllers/dreams.py
+++ flask_app/controllers/dreams.py
@@ -1,5 +1,5 @@
 from flask import render_template, redirect, session, request
-from flask_app import app
+from __init__ import app
 from flask_app.models.dream import Dream
 from flask_app.models.user import User
 
