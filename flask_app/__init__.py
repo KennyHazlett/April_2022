359ff6c43ecbbbf05a002eeb792212a7ef31a053
Kenneth Hazlett <hazlettkenneth1@gmail.com>
2022-06-20 18:57:53 -0400

    round26

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -1,5 +1,9 @@
 from flask import Flask
-from flask import render_template, redirect, session, request, flash
+from flask import render_template
+from flask_app.controllers import comments, dreams, users
+from flask_app.models import comment, dream, user
+from flask_app.templates import content, edit_content, edit_dream, login, menu, record, registration
+
 
 app = Flask(__name__)
 
@@ -7,14 +11,14 @@ app = Flask(__name__)
 app.secret_key = "Kenny."
 
 
-@app.route("/")
-def home_view():
-    return render_template("registration.html")
+# @app.route("/")
+# def home_view():
+#     return render_template("registration.html")
 
 
-@app.route("/login/page")
-def login_page():
-    return render_template("login.html")
+# @app.route("/login/page")
+# def login_page():
+#     return render_template("login.html")
 
 
 # @app.route("/")
--- flask_app/controllers/dreams.py
+++ flask_app/controllers/dreams.py
@@ -1,4 +1,4 @@
-from flask import render_template, redirect, session, request, flash
+from flask import render_template, redirect, session, request
 from flask_app import app
 from flask_app.models.dream import Dream
 from flask_app.models.user import User
