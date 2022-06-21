4c64ab8d6e9932f13e25b68095ee8f178fa04a37
Kenneth Hazlett <hazlettkenneth1@gmail.com>
2022-06-20 20:18:32 -0400

    round33

--- flask_app/models/user.py
+++ flask_app/models/user.py
@@ -16,6 +16,7 @@ class User:
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
 
+
     @classmethod
     def save(cls, data):
         query = "INSERT INTO users (first_name,last_name,email,password) VALUES(%(first_name)s,%(last_name)s,%(email)s,%(password)s)"
